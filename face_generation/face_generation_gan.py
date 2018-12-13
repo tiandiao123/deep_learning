5117d2b0bd2989b6bb31b1c310b873b170cb3e4b
Cuiqing Li <cqli@yitu-inc.intra>
2018-12-14 00:41:00 +0800

    trained sucessfully, but needs to be done adjust parameters

--- face_generation/face_generation_gan.py
+++ face_generation/face_generation_gan.py
@@ -6,6 +6,17 @@ from distutils.version import LooseVersion
 import warnings
 import tensorflow as tf
 import numpy as np
+import math
+import os
+import hashlib
+from urllib.request import urlretrieve
+import zipfile
+import gzip
+import shutil
+
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
 
 # Check TensorFlow Version
 assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
@@ -39,12 +50,12 @@ def discriminator(images, reuse=False):
 
 		# 14*14*64
 		layer2 = tf.layers.conv2d(relu1, 128, 5, strides =2, padding='same')
-		layer2 = tf.layer2.batch_normalization(layer2, training = True)
+		layer2 = tf.layers.batch_normalization(layer2, training = True)
 		relu2 = tf.maximum(0.12*layer2, layer2)
 
 		# 7*7*128
 		layer3 = tf.layers.conv2d(relu2, 256, 5, strides=2, padding='same')
-		layer3 = tf.layers.batch_normalization(layers, training= True)
+		layer3 = tf.layers.batch_normalization(layer3, training= True)
 		relu3 = tf.maximum(0.12*layer3, layer3)
 
 		flatten_layer = tf.reshape(relu3, (-1, 4*4*256))
@@ -65,7 +76,7 @@ def generator(z, out_channel_dim, is_train = True):
 
 		# input 7*7*512
 		layer2 = tf.layers.conv2d_transpose(relu1, 256, 5, strides=1, padding='same')
-		layer2 = tf.layer2.batch_normalization(layer2, training = is_train)
+		layer2 = tf.layers.batch_normalization(layer2, training = is_train)
 		relu2 = tf.maximum(0.12*layer2, layer2)
 
 		#output 7*7*256
@@ -75,7 +86,7 @@ def generator(z, out_channel_dim, is_train = True):
 		relu3 = tf.maximum(0.12*layer3, layer3)
 		# 14 * 14 * 128 
 
-		logits = tf.layers.conv2d_transpose(relu3, out_channel_dim, 5, strides=2. padding='same')
+		logits = tf.layers.conv2d_transpose(relu3, out_channel_dim, 5, strides=2, padding='same')
 		output = tf.tanh(logits)
 
 	return output
@@ -97,11 +108,11 @@ def model_loss(input_real, input_z, out_channel_dim):
 def model_opt(d_loss, g_loss, learning_rate, beta1):
 	t_vars = tf.trainable_variables()
 	d_vars = [var for var in t_vars if var.name.startswith('discriminator')]
-	g_vars = [var for vat in t_vars if var.name.startswith('generator')]
+	g_vars = [var for var in t_vars if var.name.startswith('generator')]
 
 	with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
-		d_train_opt = tf.train.AdamOptimizer(learning_rate, beta1=beta).minimize(d_loss, var_list=d_vars)
-		g_train_opt = tf.train.AdamOptimizer(learning_rate, beta1=beta).minimize(g_loss, var_list=g_vars)
+		d_train_opt = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(d_loss, var_list=d_vars)
+		g_train_opt = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(g_loss, var_list=g_vars)
 	return d_train_opt, g_train_opt
 
 
@@ -124,28 +135,32 @@ def show_generator_output(sess, n_images, input_z, out_channel_dim, image_mode):
         feed_dict={input_z: example_z})
 
     images_grid = helper.images_square_grid(samples, image_mode)
-    pyplot.save_config("step_" + str(count) + ".png")
+    images_grid.save("./images_examples/step_" + str(count) + ".png")
 
 
 
 
 def train(epoch_count, batch_size, z_dim, learning_rate, beta1, get_batches, data_shape, data_image_mode):
-	input_real, input_z, lr = model_inputs(data_shape[1], data_shape[2], data_shape[3])
+	input_real, input_z, lr = model_inputs(data_shape[1], data_shape[2], data_shape[3], z_dim)
 	d_loss, g_loss = model_loss(input_real, input_z, data_shape[3])
 	d_optimizer, g_optimizer = model_opt(d_loss, g_loss, lr, beta1)
 
 	with tf.Session() as sess:
-		for epch_i in range(len(epoch_count)):
+		sess.run(tf.global_variables_initializer())
+		for epoch_i in range(epoch_count):
 			step = 0
 			for batch_images in get_batches(batch_size):
 				batch_images = 2*batch_images
 				step+=1
-				batch_z = np.random.uniform(-1, 1, size = (batch_z, z_dim))
+				batch_z = np.random.uniform(-1, 1, size = (batch_size, z_dim))
 				_ = sess.run(d_optimizer, feed_dict = {input_real:batch_images, input_z: batch_z, lr: learning_rate})
 
 				if step%10 == 0:
 					train_loss_d = d_loss.eval({input_real:batch_images, input_z: batch_z})
 					train_loss_g = g_loss.eval({input_z: batch_z})
+					print("Epoch {}".format(epoch_i),
+						"The No. {} steps loss_d is {}".format(step,train_loss_d),
+						"The No. {} steps loss_g is {}".format(step,train_loss_g))
 
 				if step%100 == 0:
 					count = step
