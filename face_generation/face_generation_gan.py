02975ace0f6487f114aba28bf3518b560e7ebfce
Cuiqing Li <cqli@yitu-inc.intra>
2018-12-13 20:53:01 +0800

    add discrminator function

--- face_generation/face_generation_gan.py
+++ face_generation/face_generation_gan.py
@@ -17,3 +17,35 @@ else:
     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 
 print("hello test!")
+
+
+def model_inputs(image_width, image_height, image_channels, z_dim):
+	input_real = tf.placeholder(tf.float32, (None, image_width, image_height, image_channels), name="input_real")
+	input_z = tf.placeholder(tf.float32, (None, z_dim), name = "input_z")
+	learning_rate = tf.placeholder(tf.float32, name = "learning_rate")
+
+	return input_real, input_z, learning_rate
+
+def discriminator(images, reuse=False):
+	
+	with tf.variable_scope('discriminator', reuse = reuse):
+		# input size 28*28*3
+		layer1 = tf.layers.conv2d(images, 64, 5, strides = 2, padding='same')
+		relu1 = tf.maximum(0.12*layer1, layer1)
+
+		# 14*14*64
+		layer2 = tf.layers.conv2d(relu1, 128, 5, strides =2, padding='same')
+		layer2 = tf.layer2.batch_normalization(layer2, training = True)
+		relu2 = tf.maximum(0.12*layer2, layer2)
+
+		# 7*7*128
+		layer3 = tf.layers.conv2d(relu2, 256, 5, strides=2, padding='same')
+		layer3 = tf.layers.batch_normalization(layers, training= True)
+		relu3 = tf.maximum(0.12*layer3, layer3)
+
+		flatten_layer = tf.reshape(relu3, (-1, 4*4*256))
+		flatten_layer = tf.layers.dropout(flatten_layer, rate = 0.3)
+		logits = tf.layers.dense(flatten_layer, 1)
+
+		return out, logits
+
