bae1f14de9071d0286897f6a7232ff235dc2794d
Cuiqing Li <cqli@yitu-inc.intra>
2019-01-30 22:17:26 +0800

    add pytorch Project

---
+++ PyTorch_Tutorial/NLP_project/classify_names.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals, print_function, division
+from io import open
+import glob
+import os
+
+def findFiles(path): return glob.glob(path)
+
+print(findFiles('data/names/*.txt'))
+
+
